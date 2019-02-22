f9403b59467b2cf046d0b0247140872b3c7d45d0
Satu <satu.salekari@sc5.io>
2019-02-22 14:23:01 +0200

    Server CORS change
    Allow all origin for webpapi.py.

--- server/webapi.py
+++ server/webapi.py
@@ -1,22 +1,11 @@
 import json
+from lambda_decorators import cors_headers
 
 MOCK_DATA = [
     {"message": "Hello from lambda 1"},
     {"message": "Hello from lambda 2"}
 ]
 
+@cors_headers
 def handler(event, context):
-    return {"statusCode": 200, "body": json.dumps(MOCK_DATA)}
-
-{
-  "Version":"2012-10-17",
-  "Statement":[
-    {
-      "Sid":"AddPerm",
-      "Effect":"Allow",
-      "Principal": "*",
-      "Action":["s3:GetObject"],
-      "Resource":["arn:aws:s3:::serverless-react-template-frontend/*"]
-    }
-  ]
-}
+    return {"statusCode": 200,  "body": json.dumps(MOCK_DATA)}
