4e64a7429dac26f6ac301201ceca7ffa524ed66d
Satu <satu.salekari@sc5.io>
2019-03-03 13:45:20 +0200

    Change the backend url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -3,7 +3,7 @@ const config = {
     BACKEND_URL: 'http://localhost:3001',
   },
   production: {
-    BACKEND_URL: 'https://byqoa7pqz5.execute-api.eu-north-1.amazonaws.com/dev',
+    BACKEND_URL: 'https://a9dvyhc6ej.execute-api.eu-north-1.amazonaws.com/dev',
   },
 };
 export default config[process.env.NODE_ENV];
